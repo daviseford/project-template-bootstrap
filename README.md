70981dae7525a3bcd61ef484a2af91fed822e62b
Davis Ford <davis.e.ford@gmail.com>
2015-11-11 11:00:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Includes  
+ 
+JS  
+string.js  
+
+
+PHP  
+simple_html_dom for HTML manipulation
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Davis
+ * Date: 11/11/2015
+ * Time: 10:45 AM
+ * Purpose: Here's a default testbed with jQuery, Bootstrap, and some typical JS files
+ */
+?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Responsive HTML Email Generator</title>
+
+    <!-- jQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+</head>
+
+<body>
+
+
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-12">
+        </div>
+    </div>
+
+    <div class="row clearfix"></div>
+
+</div>
+</body>
+</html>
